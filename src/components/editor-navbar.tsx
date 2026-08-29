d43df7181115c4d0ab9d0d05f97811d3863a50bd
Andreina Di Brigida <Akiarakirax3@gmail.com>
2026-08-28 21:56:47 -0400

    Adding pwa install hint

--- src/components/editor-navbar.tsx
+++ src/components/editor-navbar.tsx
@@ -32,6 +32,7 @@ import {
 } from "@/components/ui/alert-dialog";
 import { SplitButton } from "@/components/shadcn-blocks/split-button";
 import { ThemeToggle } from "@/components/theme-toggle";
+import { InstallPwaHint } from "@/components/install-pwa-hint";
 import type { QrDownloadFormat } from "@/lib/qr-utils";
 import type { VCardVersion } from "@/types/vcard-types";
 
@@ -207,6 +208,15 @@ export function EditorNavbar({
               />
             </div>
           </div>
+
+          <InstallPwaHint
+            variant="outline"
+            size="sm"
+            label="Install app"
+            hintText="Ready to install"
+            installedText="App installed"
+            className="hidden sm:flex"
+          />
 
           {/* Desktop ThemeToggle */}
           <div className="hidden lg:block">
